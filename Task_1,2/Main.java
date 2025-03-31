5cc4d5d9522809f4add56b30a985ebdcd32da671
Aasmi Kothari <90106410+Aasmikothari@users.noreply.github.com>
2025-03-31 11:36:27 +0530

    Add files via upload

---
+++ Task_1,2/Main.java
@@ -0,0 +1,22 @@
+package com.techshop.entity;
+public class Main {
+	public static void main(String[] args) {
+        Customer customer1 = new Customer(1, "John", "Doe", "john.doe@email.com", "1234567890", "123 Street");
+        Product product1 = new Product(101, "Smartphone", "Latest model smartphone", 699.99);
+        Inventory inventory1 = new Inventory(1, product1, 50);
+        Order order1 = new Order(1001, customer1);
+        OrderDetail orderDetail1 = new OrderDetail(1, order1, product1, 2);
+
+        System.out.println("--- Customer Details ---");
+        customer1.getCustomerDetails();
+        
+        System.out.println("\n--- Product Details ---");
+        product1.getProductDetails();
+        
+        System.out.println("\n--- Order Details ---");
+        order1.getOrderDetails();
+        
+        System.out.println("\n--- Inventory Stock ---");
+        System.out.println("Stock Available: " + inventory1.getQuantityInStock());
+    }
+}
