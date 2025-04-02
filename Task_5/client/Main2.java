292652611b313bf4327ae84bac7f564f3b6d2ee1
Aasmi Kothari <90106410+Aasmikothari@users.noreply.github.com>
2025-04-02 19:14:04 +0530

    Update Main2.java

--- Task_5/client/Main2.java
+++ Task_5/client/Main2.java
@@ -11,25 +11,21 @@ public class Main2 {
         System.out.println("Initial stock: " + inventory.getQuantityInStock());
 
         try {
-            // Removing valid stock
             inventory.removeFromInventory(5);
             System.out.println("Stock after removing 5: " + inventory.getQuantityInStock());
 
-            // Attempting to remove more than available
             inventory.removeFromInventory(6);
         } catch (InsufficientStockException e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            // Setting negative stock
             inventory.setQuantityInStock(-5);
         } catch (InvalidDataException e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            // Trying to remove zero or negative quantity
             inventory.removeFromInventory(0);
         } catch (InvalidDataException e) {
             System.out.println(e.getMessage());
